dd5132718d54babb00afb344d3ab0ba1dd0d07d0
Tom Rook <93784825+TomRook12@users.noreply.github.com>
2022-03-13 10:16:15 +0000

    Start of kivy UI

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
 from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.factory import Factory
+from kivy.properties import ObjectProperty
+from kivy.core.window import Window
+from kivy.uix.popup import Popup
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 
-Builder.load_file("converter.kv")
+import os
 
-class MyLayout(Widget):
-    pass
+Window.size = (1920, 1080)
+
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class SaveDialog(FloatLayout):
+    save = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+class Root(FloatLayout):
+    loadfile = ObjectProperty(None)
+    savefile = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+
+    def selected(self, filename):
+        try:
+            self.ids.my_image.source = filename[0]
+            #print(filename[0])
+
+        except:
+            pass
 
 class ConverterApp(App):
-    def build(self):
-        return MyLayout()
+    pass
+
+Factory.register('Root', cls=Root)
+Factory.register('LoadDialog', cls=LoadDialog)
+Factory.register('SaveDialog', cls=SaveDialog)
 
 if __name__ == "__main__":
     ConverterApp().run()
 
-#https://www.youtube.com/watch?v=YlRd4rw_vBw&ab_channel=Codemy.com
